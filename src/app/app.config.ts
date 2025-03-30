34f9da4111f2818a7226604d6fffbce27183ca80
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-30 23:49:05 +0500

    feat(auth): Add auth0 for authentication
    - Implement auth0 authentication
    - Implement redirect

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
+import { environment } from '../environments/environment.development';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideAuth0({
+      domain: environment.auth.domain,
+      clientId: environment.auth.clientId,
+      authorizationParams: {
+        redirect_uri: environment.authorizationParams.redirectUri
+      }
+    }),
+  ],
 };

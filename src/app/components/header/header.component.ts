ad11f9be99adc9551c284934ea2893e75bd55d20
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-04-01 23:16:07 +0500

    feat(ui): add header
    - implement header for seamless navigation
    - add spartan-ng brain and helm libraries

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,111 @@
+import { Component, inject, signal, input } from '@angular/core';
+import { DOCUMENT, NgFor, TitleCasePipe } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+import { BrnMenuTriggerDirective } from '@spartan-ng/brain/menu';
+import {
+  HlmMenuComponent,
+  HlmMenuGroupComponent,
+  HlmMenuItemDirective,
+  HlmMenuItemIconDirective,
+  HlmMenuLabelComponent,
+  HlmMenuSeparatorComponent,
+  HlmMenuShortcutComponent,
+} from '@spartan-ng/ui-menu-helm';
+
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  lucideBookmark,
+  lucideCog,
+  lucideHeart,
+  lucideLayers,
+  lucideLayoutDashboard,
+  lucideLogIn,
+  lucideLogOut,
+  lucideUser,
+  lucideUserPlus,
+} from '@ng-icons/lucide';
+import { AsyncPipe, NgClass, NgIf } from '@angular/common';
+
+// Import the AuthService type from the SDK
+import { AuthService } from '@auth0/auth0-angular';
+
+
+export interface Menu {
+  name: string;
+  link: string;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-header',
+  imports: [    
+    RouterLink,
+    RouterLinkActive,
+    AsyncPipe,
+    NgClass,
+    NgIcon,
+    HlmIconDirective,
+    HlmMenuComponent,
+    HlmMenuGroupComponent,
+    HlmMenuItemDirective,
+    HlmMenuItemIconDirective,
+    HlmMenuLabelComponent,
+    HlmMenuSeparatorComponent,
+    HlmMenuShortcutComponent,
+    BrnMenuTriggerDirective,
+  ],
+  providers: [
+    provideIcons({
+      lucideLayoutDashboard,
+      lucideHeart,
+      lucideBookmark,
+      lucideLogIn,
+      lucideUserPlus,
+      lucideUser,
+      lucideLayers,
+      lucideCog,
+      lucideLogOut,      
+    }),
+  ],
+  templateUrl: './header.component.html',
+  styles: `
+  `,
+})
+export class HeaderComponent {
+  
+  public document: Document = inject(DOCUMENT);
+  public auth: AuthService = inject(AuthService);
+  user = this.auth.user$;
+  public isLoading = this.auth.isLoading$;
+  
+  public isLoggingOut = signal(false);
+
+
+
+  constructor() {
+    this.user.subscribe((user) => {
+      console.log('Current user:', user);
+    });
+  }
+
+  menu = signal<Menu[]>([
+    {
+      name: 'Browse',
+      link: '/',
+      icon: 'lucideLayoutDashboard',
+    },
+    {
+      name: 'Favorites',
+      link: '/favorites',
+      icon: 'lucideHeart',
+    },
+    {
+      name: 'Saved',
+      link: '/bookmarks',
+      icon: 'lucideBookmark',
+    },
+  ]);
+  
+}

c80fa9171751c85928802ef195bbd0175f00c9aa
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-04-04 01:09:59 +0500

    refactor: remove unknown paths to not found page
    - implement not found route
    - remove the unknown paths wildcard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component : HomeComponent,
+        pathMatch: 'full'
+    },
+    // {
+    //     path: 'favorites',
+    //     loadComponent: () => import('./pages/favorites/favorites.component').then(m => m.FavoritesComponent),
+    // },
+    {
+        path: 'bookmarks',
+        loadComponent: () => import('./pages/bookmarks/bookmarks.component').then(m => m.BookmarksComponent)
+    },
+    {
+       path: 'pokemon/:id',
+       loadComponent: () => import('./pages/pokemon-details/pokemon-details.component').then(m => m.PokemonDetailsComponent)
+    },
+    // {
+    //     path: '**',
+    //     redirectTo: '',
+    //     pathMatch: 'full',        
+    // }
+    {
+        path: '**',
+        loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent)
+    }
+];

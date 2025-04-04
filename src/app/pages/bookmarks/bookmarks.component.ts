c73a6074a4b062f36e5c1f989df80ba3c547f4ee
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-04-05 01:07:29 +0500

    todo(ui): implement bookmarks page
    - add protected routes
    - add authorizations
    - sync with db

---
+++ src/app/pages/bookmarks/bookmarks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bookmarks',
+  imports: [],
+  templateUrl: './bookmarks.component.html',
+  styles: ``
+})
+export class BookmarksComponent {
+
+}

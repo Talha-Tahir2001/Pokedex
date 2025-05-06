f75a86965104bf239140efe44686f9d0e0a9399f
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-05-07 01:13:52 +0500

    chore: load environment variables dynamically from env

---
+++ mynode.js
@@ -0,0 +1,44 @@
+import { writeFile } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+// Simulate __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '..'); // Move to the directory
+
+// Load environment variables from .env
+dotenv.config({ path: 'src/.env' });
+
+// Environment file content
+const envFile = `export const environment = {
+    production: true,
+    auth: {
+        domain: '${process.env.domain}',
+        clientID: '${process.env.clientID}',
+    },
+    authorizationParams: {
+        redirectUri: window.location.origin,
+    },
+    api: {
+        POKE_API_URI: '${process.env.POKE_API_URL}',
+        BACKEND_API_URI: '${process.env.BACKEND_API_URI}',
+        API_URI_BOOKMARKS: '${process.env.API_URI_BOOKMARKS}',
+    },  
+};
+`;
+
+// Define the target path
+const targetPath = join(__dirname, './src/environments/environment.ts');
+
+// Write the file
+writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        const successColor = '\x1b[32m%s\x1b[0m';
+        const checkSign = '\u{2705}';
+        console.log(successColor, `${checkSign} Successfully generated environment.ts`);
+    }
+});

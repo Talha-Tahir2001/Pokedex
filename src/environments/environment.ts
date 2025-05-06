f18da2cec2c07ea8b91c68460c3e82ea8bf065be
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-05-07 01:08:36 +0500

    chore: add environment config for deployment

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    production: true,    
+    auth: {
+        domain: 'Your Auth0 Domain',
+        clientID: 'Your Auth0 Client ID',
+    },
+    authorizationParams: {
+        redirectUri: window.location.origin,
+    },    
+    api: {
+        POKE_API_URI: 'PokeAPI URI',
+        BACKEND_API_URI: 'Your Backend API URI',        
+        API_URI_BOOKMARKS: 'Your Bookmarks API URI/Bookmarks',
+    },   
+};
